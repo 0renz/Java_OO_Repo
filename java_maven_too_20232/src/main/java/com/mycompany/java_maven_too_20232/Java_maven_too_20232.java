e26486d54cfd7ece803e070d64236d81ab20fcf2
loren <loren@DESKTOP-HO5IAL7>
2023-08-14 16:25:44 -0300

    teste_1

---
+++ java_maven_too_20232/src/main/java/com/mycompany/java_maven_too_20232/Java_maven_too_20232.java
@@ -0,0 +1,8 @@
+package com.mycompany.java_maven_too_20232;
+
+public class Java_maven_too_20232 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

8757d86146cb5063f032a4fbe621e558d87e58c0
20222PF.CC0021 <20222PF.CC0021@pc-155963.passofundo.ifsul.local>
2023-08-21 09:38:01 -0300

    teste

--- java_maven_too_20232/src/main/java/com/mycompany/java_maven_too_20232/Java_maven_too_20232.java
+++ java_maven_too_20232/src/main/java/com/mycompany/java_maven_too_20232/Java_maven_too_20232.java
@@ -3,6 +3,6 @@ package com.mycompany.java_maven_too_20232;
 public class Java_maven_too_20232 {
 
     public static void main(String[] args) {
-        System.out.println("Hey World, thats me");
+        System.out.println("teste");
     }
 }
